c896740436dfb73ce9de9c5151c0b07c79d3c4d5
lburgazzoli <lburgazzoli@gmail.com>
2018-08-06 14:44:35 +0200

    example

--- src/main/java/com/github/lburgazzoli/graal/Main.java
+++ src/main/java/com/github/lburgazzoli/graal/Main.java
@@ -1,68 +1,32 @@
 package com.github.lburgazzoli.graal;
 
-import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 import org.graalvm.polyglot.proxy.ProxyExecutable;
 import org.graalvm.polyglot.proxy.ProxyObject;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try(Context ctx = Context.create()) {
-            final MyBean bean = new MyBean();
-            final MyBeanProxy proxy = new MyBeanProxy(bean);
-
-            ctx.getBindings("js").putMember("bean", proxy);
-
-            ctx.eval(
-                Source.newBuilder("js", new File(args[0])).build()
-            );
-        }
-    }
-
-    public static class MyBeanProxy implements ProxyObject {
-        private final MyBean bean;
-
-        public MyBeanProxy(MyBean bean) {
-            this.bean = bean;
-        }
-
-        @Override
-        public Object getMember(String key) {
-            if ("method".equals(key)) {
-                return new ProxyExecutable() {
-                    @Override
-                    public Object execute(Value... arguments) {
-                        bean.method();
-                        return null;
+            final Map<String, Object> proxy = new HashMap<>();
+            proxy.put("sayHello", new ProxyExecutable() {
+                @Override
+                public Object execute(Value... arguments) {
+                    if (arguments.length != 1) {
+                        throw new IllegalArgumentException();
                     }
-                };
-            }
 
-            return null;
-        }
+                    System.out.printf("Hello, %s\n", arguments[0].asString());
+                    return null;
+                }
+            });
 
-        @Override
-        public Object getMemberKeys() {
-            return new String[] { "method" };
-        }
+            ctx.getBindings("js").putMember("bean", ProxyObject.fromMap(proxy));
 
-        @Override
-        public boolean hasMember(String key) {
-            return "method".equals(key);
-        }
-
-        @Override
-        public void putMember(String key, Value value) {
-            throw new UnsupportedOperationException("removeMember() not supported.");
-        }
-    }
-
-    public static class MyBean {
-        public void method() {
-            System.out.println("MyBean::method");
+            ctx.eval("js", "bean.sayHello('World!')");
         }
     }
 }
